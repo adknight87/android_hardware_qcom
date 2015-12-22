0b3bfa7ac21b4927ba3be04076690ae74fab7185
jgfarnden <jgfarnden@gmail.com>
2015-12-22 01:53:35 -0700

    First Commit

---
+++ display-caf-new/Android.mk
@@ -0,0 +1,25 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+
+ifeq ($(TARGET_HAVE_NEW_GRALLOC),true)
+    display-hals := libgralloc
+else
+    display-hals := libgralloc-compat
+endif
+
+display-hals += libcopybit libvirtual
+display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
+display-hals += libmemtrack
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    include $(call all-named-subdir-makefiles,$(display-hals))
+else
+ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+    #This is for mako since it doesn't have the QCOM make functions
+    include $(call all-named-subdir-makefiles,$(display-hals))
+endif
+endif
+endif
